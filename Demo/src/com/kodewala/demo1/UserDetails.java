329bbc10c54f8907e53280c2fd8916e1de37b980
Abinash <nabinash9247@gmail.com>
2026-01-05 18:55:22 +0530

    add new project

---
+++ Demo/src/com/kodewala/demo1/UserDetails.java
@@ -0,0 +1,10 @@
+package com.kodewala.demo1;
+
+public class UserDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Abinash");
+	}
+
+}
