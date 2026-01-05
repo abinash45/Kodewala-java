e11ff0028682c66a49bb72ab8a57e810d8a5de60
Abinash <nabinash9247@gmail.com>
2026-01-05 18:42:02 +0530

    Updated logic in Main class

---
+++ ExceptionHandling/src/com/kodewala/exceptions/Driver2.java
@@ -0,0 +1,10 @@
+package com.kodewala.exceptions;
+
+public class Driver2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("Hello world");
+	}
+
+}
